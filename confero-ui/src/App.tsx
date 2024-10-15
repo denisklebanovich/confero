81f938ead286fe781e9f71a3b72ec01016187696
gguser <denis.klebanovich@gmail.com>
2024-10-15 18:49:59 +0200

    Supabase

--- confero-ui/src/App.tsx
+++ confero-ui/src/App.tsx
@@ -1,8 +1,22 @@
+import {Auth} from "@supabase/auth-ui-react";
+import {supabase} from "@/auth/supabaseClient.ts";
+import {ThemeSupa} from "@supabase/auth-ui-shared";
+
 function App() {
     return (
-        <>
+        <Auth supabaseClient={supabase} appearance={{theme: ThemeSupa}}
+              providers={['linkedin_oidc']}
+              onlyThirdPartyProviders
+              localization={{
+                  variables: {
+                      sign_in:{
+                          social_provider_text: 'Zaloguj się przez LinkedIn'
+                      }
+                  }
+              }}
+        >
             <div>Confero UI</div>
-        </>
+        </Auth>
     )
 }
 
