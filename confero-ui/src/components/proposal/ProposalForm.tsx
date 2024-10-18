3d2335062d3caedd4eef709ad3a669cb6ff2bab1
artsiomshshshsk <artsiomshablinskiy@gmail.com>
2024-10-18 19:57:59 +0200

    react hook form + zod

--- confero-ui/src/components/proposal/ProposalForm.tsx
+++ confero-ui/src/components/proposal/ProposalForm.tsx
@@ -1,12 +1,79 @@
-import {LabelInput} from "@/components/ui/labelInput.tsx";
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+
+const formSchema = z.object({
+  title: z.string().min(2).max(50),
+  description: z.string().min(2).max(50),
+})
 
 const ProposalForm = () => {
-    return (
-        <form className='flex flex-col items-start'>
-            <LabelInput label='Title'/>
-            <LabelInput label='Description'/>
-        </form>
-    );
+
+    const form = useForm<z.infer<typeof formSchema>>({
+      resolver: zodResolver(formSchema),
+      defaultValues: {
+        title: "",
+        description: "",
+      },
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+      console.log(values)
+    }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Special Session Title</FormLabel>
+              <FormControl>
+                <Input placeholder="title" {...field} />
+              </FormControl>
+              <FormDescription>
+                some description here
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Input placeholder="description" {...field} />
+              </FormControl>
+              <FormDescription>
+                some description here
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  )
 };
 
 export default ProposalForm;
