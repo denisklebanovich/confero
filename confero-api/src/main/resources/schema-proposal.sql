ed7a0a3e1349c9aae7ae0bb739a3a8fc573cb966
artsiomshshshsk <artsiomshablinskiy@gmail.com>
2024-10-25 19:17:45 +0200

    v2

--- confero-api/src/main/resources/schema-proposal.sql
+++ confero-api/src/main/resources/schema-proposal.sql
@@ -1,12 +1,11 @@
 CREATE TABLE IF NOT EXISTS users
 (
-    id    BIGSERIAL PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) PRIMARY KEY,
     role  VARCHAR(255) NOT NULL,
     CONSTRAINT users_role_check CHECK (role IN ('ADMIN', 'USER', 'PARTICIPANT'))
 );
 
-CREATE TABLE IF NOT EXISTS proposal
+CREATE TABLE IF NOT EXISTS session
 (
     id          BIGSERIAL PRIMARY KEY,
     title       VARCHAR(255)                NOT NULL,
@@ -16,67 +15,56 @@ CREATE TABLE IF NOT EXISTS proposal
     description VARCHAR(40000)              NOT NULL,
     status      VARCHAR(255)                NOT NULL,
     created_at  TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    CONSTRAINT proposal_type_check CHECK (type IN ('TALK', 'POSTER', 'WORKSHOP')),
-    CONSTRAINT proposal_status_check CHECK (status IN ('PENDING', 'DRAFT', 'REJECTED', 'ACCEPTED', 'CHANGE_REQUESTED')),
-    FOREIGN KEY (creator_id) REFERENCES users (id)
+    CONSTRAINT session_type_check CHECK (type IN ('SESSION', 'WORKSHOP', 'TUTORIAL')),
+    CONSTRAINT session_status_check CHECK (status IN ('PENDING', 'DRAFT', 'REJECTED', 'ACCEPTED', 'CHANGE_REQUESTED')),
+    FOREIGN KEY (creator_id) REFERENCES users (email)
 );
 
 
-CREATE TABLE IF NOT EXISTS comment
+CREATE TABLE IF NOT EXISTS proposal_comment
 (
-    id          BIGSERIAL PRIMARY KEY,
-    proposal_id BIGINT                      NOT NULL,
-    user_id     BIGINT                      NOT NULL,
-    content     VARCHAR(512)                NOT NULL,
-    created_at  TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    FOREIGN KEY (proposal_id) REFERENCES proposal (id),
-    FOREIGN KEY (user_id) REFERENCES users (id)
+    id         BIGSERIAL PRIMARY KEY,
+    session_id BIGINT                      NOT NULL,
+    user_id    BIGINT                      NOT NULL,
+    content    VARCHAR(512)                NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    FOREIGN KEY (session_id) REFERENCES session (id),
+    FOREIGN KEY (user_id) REFERENCES users (email)
 );
 
 
-CREATE TABLE IF NOT EXISTS proposal_presenters
+CREATE TABLE IF NOT EXISTS presenter
 (
-    proposal_id  BIGINT       NOT NULL,
-    presenter_id BIGINT       NOT NULL,
-    orcid        VARCHAR(255) NOT NULL,
-    PRIMARY KEY (proposal_id, presenter_id),
-    FOREIGN KEY (proposal_id) REFERENCES proposal (id),
-    FOREIGN KEY (presenter_id) REFERENCES users (id)
+    id         BIGSERIAL PRIMARY KEY,
+    email      BIGINT       NOT NULL,
+    session_id BIGINT       NOT NULL,
+    orcid      VARCHAR(255) NOT NULL,
+    name       VARCHAR(255) NOT NULL,
+    surname    VARCHAR(255) NOT NULL,
+    FOREIGN KEY (session_id) REFERENCES session (id),
+    FOREIGN KEY (email) REFERENCES users (email)
 );
 
-CREATE TABLE IF NOT EXISTS session
+CREATE TABLE IF NOT EXISTS presentation
 (
-    id          BIGSERIAL PRIMARY KEY       NOT NULL,
-    proposal_id BIGINT                      NOT NULL,
-    start_time  TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    FOREIGN KEY (proposal_id) REFERENCES proposal (id)
-);
-
-CREATE TABLE IF NOT EXISTS timetable
-(
-    id           BIGSERIAL PRIMARY KEY       NOT NULL,
-    session_id   BIGINT                      NOT NULL,
-    presenter_id BIGINT                      NOT NULL,
+    presenter_id BIGINT PRIMARY KEY,
     start_time   TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     end_time     TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    FOREIGN KEY (session_id) REFERENCES session (id),
-    FOREIGN KEY (presenter_id) REFERENCES users (id)
+    FOREIGN KEY (presenter_id) REFERENCES presenter (id)
 );
 
-CREATE TABLE IF NOT EXISTS session_attachments
+CREATE TABLE IF NOT EXISTS presenter_attachment
 (
     id           BIGSERIAL PRIMARY KEY,
-    session_id   BIGINT       NOT NULL,
+    presenter_id BIGINT       NOT NULL,
     title        VARCHAR(255) NOT NULL,
     url          VARCHAR(255) NOT NULL,
-    presenter_id BIGINT       NOT NULL,
-    FOREIGN KEY (session_id) REFERENCES session (id),
-    FOREIGN KEY (presenter_id) REFERENCES users (id)
+    FOREIGN KEY (presenter_id) REFERENCES presenter (id)
 );
 
 CREATE TABLE IF NOT EXISTS configuration
 (
     key   VARCHAR(255) PRIMARY KEY NOT NULL,
-    value VARCHAR(255)             NOT NULL,
+    value JSONB                    NOT NULL,
     CONSTRAINT configuration_key_unique UNIQUE (key)
 );
