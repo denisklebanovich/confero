cb73a8b2c5811b712f2119aaed07867fcca19b28
Vera Yemialyanava <269565@student.pwr.edu.pl>
2024-10-27 20:28:53 +0100

    add session files component

--- confero-ui/src/views/SessionView.tsx
+++ confero-ui/src/views/SessionView.tsx
@@ -1,22 +1,25 @@
-import {Link} from "react-router-dom";
-import {Button} from "@/components/ui/button.tsx";
+import { Link } from "react-router-dom";
+import { Button } from "@/components/ui/button.tsx";
 import SessionDescription from "@/components/session/SessionDescription.tsx";
 import Room from "@/components/session/Room.tsx";
+import SessionFiles from "@/components/session/SessionFiles";
 
 export default function SessionView() {
-    return (
-        <>
-            <div className={"w-full flex items-center justify-around mt-2"}>
-                <Room/>
-                <SessionDescription/>
-            </div>
+  return (
+    <>
+      <div className={"w-full flex items-center justify-around mt-2"}>
+        <Room />
+        <SessionDescription />
+      </div>
+      <div className={"w-full flex items-center justify-around mt-16"}>
+        <SessionFiles />
+      </div>
 
-            <div className={"w-full fixed bottom-12 left-0 pb-3 flex justify-center"}>
-                <Button className={"bg-gray-700 hover:bg-gray-600"}>
-                    <Link to='/'>Back</Link>
-                </Button>
-            </div>
-        </>
-
-    )
+      <div className={"w-full left-0 flex justify-center"}>
+        <Button className={"bg-gray-700 hover:bg-gray-600"}>
+          <Link to="/">Back</Link>
+        </Button>
+      </div>
+    </>
+  );
 }
