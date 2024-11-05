ab43e15ef73a95de8740fd5be532a8cbe1b70923
artsiomshshshsk <artsiomshablinskiy@gmail.com>
2024-11-05 20:43:52 +0100

    check for admin

--- confero-api/src/test/java/org/zpi/conferoapi/application/ApplicationControllerTest.java
+++ confero-api/src/test/java/org/zpi/conferoapi/application/ApplicationControllerTest.java
@@ -20,6 +20,7 @@ import static org.mockito.Mockito.when;
 class ApplicationControllerTest extends IntegrationTestBase {
 
     private static final String ORCID = "0000-0002-5678-1234";
+
     private static final String EMAIL = "example@gmail.com";
 
     @MockBean
@@ -92,28 +93,12 @@ class ApplicationControllerTest extends IntegrationTestBase {
 
         when(orcidService.getRecord(ORCID)).thenReturn(new OrcidInfoResponse().name("John").surname("Doe"));
 
-        // Prepare mock data for CreateApplicationRequest
-        var presentationRequest = new PresentationRequest()
-                .title("Introduction to AI")
-                .addPresentersItem(new PresenterRequest()
-                        .email("presenter1@example.com")
-                        .isSpeaker(true)
-                        .orcid(ORCID));
-
-        var createApplicationRequest = new CreateApplicationRequest()
-                .title("AI in Modern Science")
-                .type(SessionType.WORKSHOP)
-                .tags(List.of("AI", "Technology"))
-                .description("A deep dive into AI's impact on science")
-                .presentations(List.of(presentationRequest))
-                .saveAsDraft(false);
-
         // Execute the POST request to create an application
         var response = RestAssured
                 .given()
                 .contentType("application/json")
                 .header("Authorization", EMAIL)
-                .body(createApplicationRequest)
+                .body(createApplicationRequest())
                 .post("/api/application")
                 .then()
                 .log().ifError()
@@ -123,11 +108,11 @@ class ApplicationControllerTest extends IntegrationTestBase {
 
         // Deserialize response into ErrorResponse
         var errorResponse = response.as(ErrorResponse.class);
-       assertEquals(errorResponse.getReason(),ErrorReason.NO_ACTIVE_CONFERENCE_EDITION);
+        assertEquals(errorResponse.getReason(), ErrorReason.NO_ACTIVE_CONFERENCE_EDITION);
     }
 
     @Test
-    void shouldGetApplication(){
+    void shouldGetApplication() {
         userRepository.save(User.builder()
                 .email(EMAIL).isAdmin(false).build());
 
@@ -139,33 +124,93 @@ class ApplicationControllerTest extends IntegrationTestBase {
 
         when(orcidService.getRecord(ORCID)).thenReturn(new OrcidInfoResponse().name("John").surname("Doe"));
 
-        // Prepare mock data for CreateApplicationRequest
-        var presentationRequest = new PresentationRequest()
-                .title("Introduction to AI")
-                .addPresentersItem(new PresenterRequest()
-                        .email(EMAIL)
-                        .isSpeaker(true)
-                        .orcid(ORCID));
-
-        var createApplicationRequest = new CreateApplicationRequest()
-                .title("AI in Modern Science")
-                .type(SessionType.WORKSHOP)
-                .tags(List.of("AI", "Technology"))
-                .description("A deep dive into AI's impact on science")
-                .presentations(List.of(presentationRequest))
-                .saveAsDraft(false);
-
-        // Execute the POST request to create an application
-        var response = RestAssured
+        RestAssured
                 .given()
                 .contentType("application/json")
                 .header("Authorization", EMAIL)
-                .body(createApplicationRequest)
+                .body(createApplicationRequest())
                 .post("/api/application")
                 .then()
                 .log().ifError()
                 .statusCode(HttpStatus.CREATED.value())
                 .extract()
                 .response();
+    }
+
+    @Test
+    void only_admin_can_review_application() {
+        var user = userRepository.save(User.builder()
+                .email(EMAIL).isAdmin(false).build());
+
+        RestAssured
+                .given()
+                .contentType("application/json")
+                .header("Authorization", EMAIL)
+                .body(new ReviewRequest().type(ReviewType.ACCEPT))
+                .post("/api/application/1/review")
+                .then()
+                .log().ifError()
+                .statusCode(HttpStatus.UNAUTHORIZED.value());
+
+
+
+        conferenceEditionRepository.save(ConferenceEdition.builder()
+                .id(1L)
+                .createdAt(Instant.now())
+                .applicationDeadlineTime(Instant.now().plus(2, DAYS))
+                .build());
+
+        when(orcidService.getRecord(ORCID)).thenReturn(new OrcidInfoResponse().name("John").surname("Doe"));
+
+        var response = RestAssured
+                .given()
+                .contentType("application/json")
+                .header("Authorization", EMAIL)
+                .body(createApplicationRequest())
+                .post("/api/application")
+                .then()
+                .log().ifError()
+                .statusCode(HttpStatus.CREATED.value())
+                .extract()
+                .response()
+                .as(ApplicationPreviewResponse.class);
+
+
+        user.setIsAdmin(true);
+        userRepository.save(user);
+
+        RestAssured
+                .given()
+                .contentType("application/json")
+                .header("Authorization", EMAIL)
+                .body(new ReviewRequest().type(ReviewType.ACCEPT))
+                .post("/api/application/" + response.getId() + "/review")
+                .then()
+                .log().ifError()
+                .statusCode(HttpStatus.OK.value());
+    }
+
+
+    private PresenterRequest presenterRequest() {
+        return new PresenterRequest()
+                .email(EMAIL)
+                .isSpeaker(true)
+                .orcid(ORCID);
+    }
+
+    private PresentationRequest presentationRequest() {
+        return new PresentationRequest()
+                .title("Introduction to AI")
+                .addPresentersItem(presenterRequest());
+    }
+
+    private CreateApplicationRequest createApplicationRequest() {
+        return new CreateApplicationRequest()
+                .title("AI in Modern Science")
+                .type(SessionType.WORKSHOP)
+                .tags(List.of("AI", "Technology"))
+                .description("A deep dive into AI's impact on science")
+                .presentations(List.of(presentationRequest()))
+                .saveAsDraft(false);
     }
 }
