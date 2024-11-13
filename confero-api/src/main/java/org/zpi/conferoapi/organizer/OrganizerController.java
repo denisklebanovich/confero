f0c8cc9da46aad0346c8c656443d61f7622bde74
Artsiom Shablinski <81441099+artsiomshshshsk@users.noreply.github.com>
2024-11-13 23:33:11 +0100

    Find orginizers (#27)
    * find organizers
    
    * tests

---
+++ confero-api/src/main/java/org/zpi/conferoapi/organizer/OrganizerController.java
@@ -0,0 +1,30 @@
+package org.zpi.conferoapi.organizer;
+
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.openapitools.api.OrganizerApi;
+import org.openapitools.model.OrganizerResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.zpi.conferoapi.security.SecurityUtils;
+
+import java.util.List;
+
+@Slf4j
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE, makeFinal = true)
+@AllArgsConstructor
+@RestController
+public class OrganizerController implements OrganizerApi {
+
+    OrganizerService organizerService;
+    SecurityUtils securityUtils;
+
+    @Override
+    public ResponseEntity<List<OrganizerResponse>> findOrganizers(String searchQuery) {
+        log.info("User {} is searching for organizers with query: {}", securityUtils.getCurrentUser(), searchQuery);
+        var organizers = organizerService.findOrganizers(searchQuery);
+        log.info("Responding with following organizers: {}", organizers);
+        return ResponseEntity.ok(organizers);
+    }
+}
---
+++ confero-api/src/test/java/org/zpi/conferoapi/organizer/OrganizerControllerTest.java
@@ -0,0 +1,132 @@
+package org.zpi.conferoapi.organizer;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+import org.openapitools.model.OrganizerResponse;
+import org.openapitools.model.SessionType;
+import org.openapitools.model.SesssionEventResponse;
+import org.springframework.http.HttpStatus;
+import org.zpi.conferoapi.IntegrationTestBase;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrganizerControllerTest extends IntegrationTestBase {
+
+    @Test
+    void findOrganizers() {
+        var session_creator_user = givenUser(
+                "0000-0002-5678-1234",
+                "access-token",
+                "http://example.com/avatar.png",
+                false,
+                List.of("session-creator@gmail.com")
+        );
+
+        var conferenceEdition = givenConferenceEdition(Instant.now().plus(1, ChronoUnit.DAYS));
+        var session = givenSession(
+                "Test Session",
+                SessionType.SESSION,
+                session_creator_user,
+                conferenceEdition,
+                "This is a test session description."
+        );
+
+        var presentation = givenPresentation(
+                "Test Presentation",
+                "Presentation Description",
+                session,
+                Instant.now().plus(1, ChronoUnit.HOURS),
+                Instant.now().plus(2, ChronoUnit.HOURS)
+        );
+
+        givenPresenter(
+                "bar@gmail.com",
+                "0000-0002-5678-1234",
+                "Artsiom",
+                "Shablinski",
+                "title 1",
+                "Politechnika Wrocławska",
+                true,
+                presentation
+        );
+
+        givenPresenter(
+                "foo@gmail.com",
+                "0000-0002-5678-1235",
+                "ArtsiBar",
+                "ShabFoo",
+                "title 1",
+                "Politechnika Wrocławska",
+                true,
+                presentation
+        );
+
+
+        var pastConferenceEdition = givenConferenceEdition(Instant.now().minus(1, ChronoUnit.DAYS));
+
+        var pastSession = givenSession(
+                "Past Test Session",
+                SessionType.SESSION,
+                session_creator_user,
+                pastConferenceEdition,
+                "This is a test session description."
+        );
+
+        var pastPresentation = givenPresentation(
+                "Past Test Presentation",
+                "Presentation Description",
+                pastSession,
+                Instant.now().minus(1, ChronoUnit.HOURS),
+                Instant.now().minus(1, ChronoUnit.MINUTES)
+        );
+
+        givenPresenter(
+                "bar@gmail.com",
+                "0000-0002-5678-1222",
+                "ArtsiFooBar",
+                "ShaFooBar",
+                "title 1",
+                "Politechnika Wrocławska",
+                true,
+                pastPresentation
+        );
+
+        givenUser(
+                "0000-0002-5678-1239",
+                "access-token1",
+                "http://example.com/avatar.png",
+                false,
+                List.of("artsi@gmail.com")
+        );
+
+
+        Function<String, OrganizerResponse[]> searchForQuery = query -> RestAssured
+                .given()
+                .contentType("application/json")
+                .header("Authorization", "artsi@gmail.com")
+                .param("searchQuery", query)
+                .get("/api/organizer")
+                .then()
+                .log().ifError()
+                .statusCode(HttpStatus.OK.value())
+                .extract()
+                .response()
+                .as(OrganizerResponse[].class);
+
+        assertEquals(1, searchForQuery.apply("Artsiom").length);
+        assertEquals(2, searchForQuery.apply("Artsi").length);
+
+        assertEquals(1, searchForQuery.apply("0000-0002-5678-1234").length);
+        assertEquals(2, searchForQuery.apply("0000-0002").length);
+
+        assertEquals(1, searchForQuery.apply("ShabFoo").length);
+        assertEquals(2, searchForQuery.apply("Shab").length);
+
+        assertEquals(2, searchForQuery.apply(null).length);
+    }
+}
