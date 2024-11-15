be4d15e881b26b99b271dc63798ece06159472b5
gguser <denis.klebanovich@gmail.com>
2024-11-15 16:51:18 +0100

    dev setup

--- confero-ui/src/api/useApiClient.ts
+++ confero-ui/src/api/useApiClient.ts
@@ -5,14 +5,14 @@ import {useMemo} from "react";
 export function useApiClient() {
     const {session, orcidAccessToken} = useAuth()
     const headers = {} as Record<string, string>
-    if (process.env.NODE_ENV === "development") {
-        headers["Authorization"] = 'denis.klebanovich@gmail.com'
-    }
     if (session?.access_token) {
         headers["Authorization"] = `Bearer ${session.access_token}`
     }
     if (orcidAccessToken) {
         headers["Orcid-Access-Token"] = orcidAccessToken
+    }
+    if (process.env.NODE_ENV === "development") {
+        headers["Authorization"] = 'denis.klebanovich@gmail.com'
     }
 
     return useMemo(() => new AppClient({
