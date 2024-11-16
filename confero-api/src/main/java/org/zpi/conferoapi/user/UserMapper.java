2a4aa7b04437a868ba0d95394cf8f27f5e76c728
artsiomshshshsk <artsiomshablinskiy@gmail.com>
2024-11-16 21:06:58 +0100

    fix build

--- confero-api/src/main/java/org/zpi/conferoapi/user/UserMapper.java
+++ confero-api/src/main/java/org/zpi/conferoapi/user/UserMapper.java
@@ -2,15 +2,23 @@ package org.zpi.conferoapi.user;
 
 import org.mapstruct.*;
 import org.openapitools.model.ProfileResponse;
+import org.openapitools.model.SessionResponse;
+import org.zpi.conferoapi.session.Session;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface UserMapper {
 
     @Mapping(target = "firstName", source = "name")
     @Mapping(target = "lastName", source = "surname")
-    @Mapping(target = "isInvitee", source = "isInvitee")
-    ProfileResponse toDto(User user, boolean isInvitee);
+    ProfileResponse toDto(User user);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     User partialUpdate(UserDto userDto, @MappingTarget User user);
+
+
+    default ProfileResponse toDto(User user, Boolean isInvitee ) {
+        ProfileResponse profileResponse = toDto(user);
+        profileResponse.setIsInvitee(isInvitee);
+        return profileResponse;
+    }
 }
