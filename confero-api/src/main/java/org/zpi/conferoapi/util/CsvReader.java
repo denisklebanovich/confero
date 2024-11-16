401dddea8946f97e31d90b72c947bc016784d200
artsiomshshshsk <artsiomshablinskiy@gmail.com>
2024-11-16 20:40:39 +0100

    fix email validation

--- confero-api/src/main/java/org/zpi/conferoapi/util/CsvReader.java
+++ confero-api/src/main/java/org/zpi/conferoapi/util/CsvReader.java
@@ -1,5 +1,11 @@
 package org.zpi.conferoapi.util;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 import org.apache.commons.csv.CSVFormat;
@@ -12,14 +18,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 @UtilityClass
 public class CsvReader {
 
-    private static final Pattern EMAIL_PATTERN = Pattern.compile(
-            "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$"
-    );
+    private static final ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+    private static final Validator validator = validatorFactory.getValidator();
 
     public static List<String> readEmailList(MultipartFile file) throws IOException {
         var emails = new ArrayList<String>();
@@ -34,14 +40,19 @@ public class CsvReader {
 
                 String email = record.get(0).trim();
                 if (!email.isEmpty()) {
-                    //TODO: ADD REGEXP VALIDATION
-//                    if (!EMAIL_PATTERN.matcher(email).matches()) {
-//                        throw new IOException("Invalid email format: " + email);
-//                    }
+
+                    EmailWrapper emailWrapper = new EmailWrapper(email);
+                    Set<ConstraintViolation<EmailWrapper>> violations = validator.validate(emailWrapper);
+
+                    if (!violations.isEmpty()) {
+                        throw new IOException("Invalid email format: " + email);
+                    }
                     emails.add(email);
                 }
             }
         }
         return emails;
     }
+
+    private record EmailWrapper(@Email(message = "Invalid email format") String email) { }
 }
