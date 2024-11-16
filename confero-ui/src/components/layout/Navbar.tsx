99899020864481a34c6723acdc257c742eafcb6b
Vera Yemialyanava <269565@student.pwr.edu.pl>
2024-11-16 09:50:15 +0100

    add profile pic to navbar

--- confero-ui/src/components/layout/Navbar.tsx
+++ confero-ui/src/components/layout/Navbar.tsx
@@ -2,11 +2,19 @@ import {Link, NavLink, useLocation, useNavigate} from "react-router-dom";
 import {Button} from "@/components/ui/button.tsx";
 import {useAuth} from "@/auth/AuthProvider.tsx";
 import {NavigationMenu, NavigationMenuItem, NavigationMenuList,} from "@/components/ui/navigationmenu.tsx";
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar.tsx";
+import {useApi} from "@/api/useApi.ts";
+import {ProfileResponse} from "@/generated";
 
 const Navbar = () => {
     const navigate = useNavigate();
     const location = useLocation();
     const {authorized, signOut} = useAuth();
+    const {apiClient, useApiQuery} = useApi();
+    const {data: profileData, isLoading} = useApiQuery<ProfileResponse>(
+        ["profile"],
+        () => apiClient.profile.getUserProfile()
+    );
 
     const routes = [
         {
@@ -36,7 +44,7 @@ const Navbar = () => {
     ]
     return (
         <div className="navbar bg-white fixed top-0 w-full z-50">
-            <div className="container my-1 flex items-center justify-between">
+            <div className="container mx-auto my-1 flex items-center justify-between">
                 <a
                     href="#"
                     className="pl-3 text-5xl font-bold text-primary"
@@ -60,13 +68,23 @@ const Navbar = () => {
                         ))}
                     </NavigationMenuList>
                 </NavigationMenu>
-                {authorized ? (
-                    <Button onClick={signOut} variant={"secondary_grey"}>Logout</Button>
-                ) : (
-                    <Button>
-                        <Link to="/login">Login</Link>
-                    </Button>
-                )}
+                <div className="flex gap-3 ml-16">
+                    {profileData && !isLoading && (
+                        <Avatar>
+                            <AvatarImage src={profileData.avatarUrl}/>
+                            <AvatarFallback>
+                                {profileData.firstName[0] + profileData.lastName[0]}
+                            </AvatarFallback>
+                        </Avatar>)
+                    }
+                    {authorized ? (
+                        <Button onClick={signOut} variant={"secondary_grey"}>Logout</Button>
+                    ) : (
+                        <Button>
+                            <Link to="/login">Login</Link>
+                        </Button>
+                    )}
+                </div>
             </div>
         </div>
     );
