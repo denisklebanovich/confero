c3051d23302b30cfc284686a3165ebd6cd06170c
gguser <denis.klebanovich@gmail.com>
2024-11-21 21:32:27 +0100

    fix

--- confero-api/src/main/java/org/zpi/conferoapi/email/EmailServiceImpl.java
+++ confero-api/src/main/java/org/zpi/conferoapi/email/EmailServiceImpl.java
@@ -3,18 +3,23 @@ package org.zpi.conferoapi.email;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.context.annotation.Bean;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EmailServiceImpl {
+
     private final JavaMailSender javaMailSender;
 
-    private static final String SUBJECT = "Confirm you E-Mail - MFA Application Registration";
+
+    private static final String SUBJECT = "Confirm you E-Mail";
     private static final String MESSAGE_CONTENT = """
             <html>
             <body> 
